aaaefb767694e585c947dccfd0c8ef8d720382de
Sri <sri@Sris-Air.lan>
2022-06-08 19:53:21 -0700

    Update to fetch images from unsplash API

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -2,60 +2,63 @@
 import Axios from 'axios';
 import React, { useState, useEffect } from "react";
 import { ChevronDoubleRightIcon, ChevronDoubleLeftIcon } from '@heroicons/react/solid'
-
+import { UNSPLASH_PUBLIC_CLIENT_ID } from "../lib/unsplash.js";
 
 function ImageSlider() {
 
   const [image, setImage] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(1);
   const length = image.length;
+  const clientID = UNSPLASH_PUBLIC_CLIENT_ID;
 
   const nextSlide = () => {
-    setCurrentSlide(currentSlide === length-1 ? 0 : currentSlide+1);
+    setCurrentSlide(currentSlide === length - 1 ? 0 : currentSlide + 1);
   };
 
   const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? length-1 : currentSlide-1);
+    setCurrentSlide(currentSlide === 0 ? length - 1 : currentSlide - 1);
   };
 
   console.log(currentSlide);
 
   useEffect(() => {
-    Axios.get("https://jsonplaceholder.typicode.com/photos").then((response) => {
-      const imgData = response.data.slice(0, 20);
-      setImage(imgData)
+    Axios.get("https://api.unsplash.com/search/collections?page=1&query=coffee&orientation=squarish&client_id=" + clientID).then((response) => {
+      console.log(response.data.results);
+      // const imgData = response.data.slice(0, 20);
+      setImage(response.data.results)
       console.log('useEffect')
     }
     );
   }, []);
-  
-  if(!Array.isArray(image) || image.length <= 0) return null;
+
+  if (!Array.isArray(image) || image.length <= 0) return null;
 
   return (
-    <section className="flex flex-row justify-center items-center p-40 relative">
-      <ChevronDoubleLeftIcon 
-      className="w-10 h-10 absolute left-10 top-[500px] cursor-pointer z-10" 
-      onClick={prevSlide}/>
-
+    <section className="grid grid-cols-3 p-28 min-h-screen relative">
+    <div></div>  
+    <div className="place-self-center">
+      <ChevronDoubleLeftIcon
+        className="w-10 h-10 absolute left-10 top-1/2 cursor-pointer z-10"
+        onClick={prevSlide} />
       {image.map((imgData, index) => {
         return (
           <div className={index === currentSlide ? 'slide active' : 'slide'} key={index}>
-          {index === currentSlide && 
-            <img  alt={imgData.title} 
-            src={imgData.url} 
-            className="w-100 h-100 rounded opacity-1"
-             />
-        }
+            {index === currentSlide &&
+              <img alt={imgData.title}
+                src={imgData.cover_photo.urls.small}
+                className="rounded"
+              />
+            }
           </div>
 
         );
       }
       )}
-      <ChevronDoubleRightIcon 
-      className="w-10 h-10 absolute right-10 top-[500px] cursor-pointer z-10"
-      onClick={nextSlide} />
-
-
+      <ChevronDoubleRightIcon
+        className="w-10 h-10 absolute right-10 top-1/2 cursor-pointer z-10"
+        onClick={nextSlide} />
+        </div>
+<div></div>
 
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
